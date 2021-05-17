3b9706fe2c20d009e1c9b2de1b079debfc63d65c
LRudL <rudolf.laine@gmail.com>
2021-05-17 21:11:37 +0100

    Pretty printing

--- src/funlisp/core.clj
+++ src/funlisp/core.clj
@@ -1,6 +1,13 @@
 (ns funlisp.core
   (:gen-class)
   (:require [funlisp.eval-simple :as evaler]
+            [funlisp.redlisp-compilation.eval-1 :as rlisp-eval1]
+            [funlisp.redlisp-compilation.eval-cps :as rlisp-eval-cps]
+            [funlisp.redlisp-compilation.eval-df :as rlisp-eval-df]
+            [funlisp.redlisp-compilation.eval-dfstack :as rlisp-eval-dfstack]
+            [funlisp.redlisp-compilation.eval-dfstep :as rlisp-eval-dfstep]
+            [funlisp.redlisp-compilation.eval-dualstack :as rlisp-eval-dualstack]
+            [funlisp.redlisp-compilation.eval-stack :as rlisp-eval-stack]
             [funlisp.parser-simple :as parser]
             [clojure.string :as string]))
 
@@ -34,6 +41,48 @@
   (println (string/join ["Reading definitions from: " fpath]))
   (slurp fpath))
 
+(defn cons?
+  [thing]
+  (and (map? thing)
+       (thing :cons?)))
+
+(declare prettify)
+
+(defn cons->list
+  [cpair]
+  (cond
+    (= 'nil cpair) '()
+    (not (map? cpair)) false
+    (= 'nil (cpair :cdr)) (list (prettify (cpair :car)))
+    true (let [rest-list (cons->list (cpair :cdr))]
+           (if rest-list
+             (cons (prettify (cpair :car))
+                   (cons->list (cpair :cdr)))
+             false))))
+
+(defn cons-prettify
+  [cpair]
+  (let [list-form (cons->list cpair)]
+    (if list-form
+      list-form
+      (list (prettify (cpair :car))
+            '.
+            (prettify (cpair :cdr))))))
+
+(defn prettify
+  [r]
+  (cond
+    (string? r) (string/join ["\"" r "\""])
+    (map? r) (cond
+               (r :function?)
+               {:function? true
+                :body (or (r :body) (r :body-exp))
+                :env "__FUNLISP_CLOSURE__"}
+               (r :cons?)
+               (cons-prettify r)
+               true r)
+    true r))
+
 (defn -main
   "Start a Funlisp REPL"
   [& args]
@@ -44,8 +93,9 @@
     ;(println init-code)
     (println "READY. Funlisp REPL; type expressions and press enter to evaluate:")
     (repl read-line
-          (fn [out] (println " >" out))
+          (fn [out] (println " >" (prettify out)))
           parser/parse
-          evaler/eval-exp-list
-          (second ; <- extract environment
-           (evaler/eval-exp-list init-code {})))))
+          rlisp-eval-dualstack/eval-exp-list ; <- change eval function here!
+          {})))
+          ;(second ; <- extract environment
+          ; (rlisp-eval1/eval-exp-list init-code {})))))
